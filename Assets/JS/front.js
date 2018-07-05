c74c00f76f3fb2a54812ec9d7e9c98dfa2b03b60
Cedric-Fournier <cedricfournier.pro@gmail.com>
2018-07-05 04:26:37 +0200

    Stroumf

---
+++ Assets/JS/front.js
@@ -0,0 +1,12 @@
+function helloButton() {
+document.getElementsByClassName('select')[0].style.display = "block";
+document.getElementsByTagName('button')[0].style.display = "block";
+document.getElementById('youtubeplayer').style.display = "inline";
+document.getElementById("upload_widget_opener").style.display = "none";
+initiate();
+}
+
+function byeByeButton() {
+document.getElementsByClassName('select')[0].style.display = "none";
+document.getElementsByTagName('button')[0].style.display = "none";
+}
